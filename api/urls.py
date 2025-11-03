25093fdd9314588becc873a79338ee308c9ddb23
ElloJhusthine <jhusthine.ello@gmail.com>
2025-11-03 14:15:21 +0800

    add password endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,19 +1,19 @@
-# api/urls.py
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SensorDataViewSet, RegisterView
+from .views import SensorDataViewSet, RegisterView, ChangePasswordView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 router = DefaultRouter()
-router.register(r'sensordata', SensorDataViewSet, basename='sensordata')
+# You can register your SensorDataViewSet with router if needed:
+# router.register('sensordata', SensorDataViewSet, basename='sensordata')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),  # ✅ New endpoint
 ]
