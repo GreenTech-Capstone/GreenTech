8b25f9e84e90e3092865377c36c1e3be4542ddff
ElloJhusthine <jhusthine.ello@gmail.com>
2025-11-21 09:31:22 +0800

    imported that cam as errors

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 
 class RegisterView(APIView):
     def post(self, request):
