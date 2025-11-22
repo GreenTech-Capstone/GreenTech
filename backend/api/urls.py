ae64c011fe6f85d524b65d18123f78ecf2c1a890
ElloJhusthine <jhusthine.ello@gmail.com>
2025-11-23 04:25:53 +0800

    SensorDataViewSet

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,8 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import SensorDataViewSet, RegisterView, ChangePasswordView, CustomTokenObtainPairView
+from django.urls import path
+from .views import RegisterView, CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
-router = DefaultRouter()
-
 urlpatterns = [
-    path('', include(router.urls)),
-
     # Register
     path('register/', RegisterView.as_view(), name='register'),
 
@@ -16,7 +11,4 @@ urlpatterns = [
 
     # Refresh token
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # Change password
-    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
