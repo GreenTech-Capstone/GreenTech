e075433699fb8058830e7a3fa6b585b5c93240ea
ElloJhusthine <jhusthine.ello@gmail.com>
2025-11-25 06:25:51 +0800

    email.py has been added

---
+++ api/utils/email.py
@@ -0,0 +1,23 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+def send_email_via_sendgrid(subject, message, to_email, from_email=None):
+    if from_email is None:
+        from_email = os.environ.get("DEFAULT_FROM_EMAIL", "jhusthine.ello@gmail.com")
+
+    mail = Mail(
+        from_email=from_email,
+        to_emails=to_email,
+        subject=subject,
+        plain_text_content=message
+    )
+
+    try:
+        sg = SendGridAPIClient(os.environ.get("EMAIL_HOST_PASSWORD"))
+        response = sg.send(mail)
+        print("SendGrid response:", response.status_code)
+        return response.status_code
+    except Exception as e:
+        print("Error sending email via SendGrid:", e)
+        return None
